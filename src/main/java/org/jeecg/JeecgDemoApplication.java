ab574e5e7545a542d33cb88a734cd391a31542dc
tanshijie <shijie.tan@shuyun.com>
2020-11-26 17:41:51 +0800

    update

--- src/main/java/org/jeecg/JeecgDemoApplication.java
+++ src/main/java/org/jeecg/JeecgDemoApplication.java
@@ -3,14 +3,25 @@ package org.jeecg;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 import java.net.UnknownHostException;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class JeecgDemoApplication {
 
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
     public static void main(String[] args) throws UnknownHostException {
+
+
         SpringApplication.run(JeecgDemoApplication.class, args);
     }
 }
--- src/main/java/org/jeecg/modules/api/controller/SystemAPIController.java
+++ src/main/java/org/jeecg/modules/api/controller/SystemAPIController.java
@@ -1,10 +1,14 @@
 package org.jeecg.modules.api.controller;
 
 import com.netflix.client.http.HttpResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -19,14 +23,24 @@ import java.util.Set;
 public class SystemAPIController {
 
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     /**
      * 发送系统消息
      */
     @GetMapping("/")
-    public void sendSysAnnouncement(HttpServletResponse httpResponse) throws IOException {
+    public String sendSysAnnouncement(HttpServletResponse httpResponse) throws IOException {
 
-        httpResponse.getWriter().println("HellWorld!");
+//        System.out.println("HellWorld!");
+//
+//        String result = restTemplate.getForObject("http://docker-test/sys/api",String.class);
+//
+//        System.out.println(result);
+        return "HellWorld!";
     }
+
+
 
 
 }
